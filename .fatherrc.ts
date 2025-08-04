fdbd945e162461cf06d2b3c711e80e127fc3f52f
ikonan <2321514568@qq.com>
2025-08-04 23:16:57 +0800

    build: 添加构建配置和发布配置
    添加 dist 目录作为模块入口，并配置 npm 发布信息

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -3,4 +3,9 @@ import { defineConfig } from 'father';
 export default defineConfig({
   esm: {},
   cjs: {},
+
+  // prebundle: {
+  //   deps: ['@ctrl/tinycolor'],
+  //   output: 'compiled',
+  // },
 });
